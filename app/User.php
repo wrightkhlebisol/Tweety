d5fa1274b74159cfad44f83ab8ba39d5e13fa58b
wright <calebogundiya@gmail.com>
2020-06-16 00:45:47 +0100

    Create follow feature and followiners list

--- app/User.php
+++ app/User.php
@@ -44,4 +44,12 @@ class User extends Authenticatable
     public function getAvatarAttribute(){
         return "https://i.pravatar.cc/40?u=" . $this->email;
     }
+
+    public function follow(User $user){
+        return $this->follows()->save($user);
+    }
+
+    public function follows(){
+        return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_user_id');
+    }
 }
