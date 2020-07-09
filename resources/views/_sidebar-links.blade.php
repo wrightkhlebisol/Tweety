06dbe2b147d5521e276ad28e70fcafb37b34acba
wright <calebogundiya@gmail.com>
2020-07-09 22:13:25 +0100

    Created symlink for file storage and logout feature

--- resources/views/_sidebar-links.blade.php
+++ resources/views/_sidebar-links.blade.php
@@ -18,16 +18,24 @@
         <a href="#" class="font-bold text-lg mb-4 block">Lists</a>
     </li>
     @auth
-        <li>
-            <a href="{{ route('profile', auth()->user()) }}" class="font-bold text-lg mb-4 block">Profile</a>
-        </li>
+    <li>
+        <a href="{{ route('profile', auth()->user()) }}" class="font-bold text-lg mb-4 block">Profile</a>
+    </li>
 
     @else
-        <li>
-            <a href="/tweets" class="font-bold text-lg mb-4 block">Profile</a>
-        </li>
+    <li>
+        <a href="/tweets" class="font-bold text-lg mb-4 block">Profile</a>
+    </li>
     @endauth
     <li>
         <a href="#" class="font-bold text-lg mb-4 block">More</a>
+    </li>
+    <li>
+        <div class="mb-6">
+            <form method="post" action="/logout">
+                @csrf
+                <button type="submit" class="bg-blue-400 text-white rounded-full py-1 px-4 hover:bg-blue-500">Logout</button>
+            </form>
+        </div>
     </li>
 </ul>
