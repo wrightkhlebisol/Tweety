80099aba6458434e4ea3375dfdf5f7160d3659e5
wright <calebogundiya@gmail.com>
2020-07-11 00:49:07 +0100

    Limit result set for pagination and format for readability

--- resources/views/_timeline.blade.php
+++ resources/views/_timeline.blade.php
@@ -1,8 +1,15 @@
 <div class="border border-gray-300 rounded-lg">
+
     @forelse($tweets as $tweet)
+
         @include('_tweet')
+
     @empty
+
         <p>No tweets yet.</p>
+
     @endforelse
+
     {{$tweets->links()}}
+    
 </div>
