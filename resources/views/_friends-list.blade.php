7bad4d372c4119c33d2058a4e088f48a7caf5eb9
wright <calebogundiya@gmail.com>
2020-07-10 21:43:36 +0100

    Fix: trailing horizontal rule in timeline

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,21 +1,23 @@
-<h3 class="font-bold text-xl mb-4">Following</h3>
+<div class="bg-gray-200 border rounded-lg py-4 px-6">
+    <h3 class="font-bold text-xl mb-4">Following</h3>
 
-<ul>
-    
-    @auth()
-        @forelse(auth()->user()->follows as $user)
-        <a href="{{route('profile', $user->username )}}">
-            <li class="mb-4">
-                <div class="flex items-center text-sm">
-                    <img src="{{ $user->avatar }}" alt="" class="rounded-full mr-2 w-10">
+    <ul>
+        
+        @auth()
+            @forelse(auth()->user()->follows as $user)
+            <a href="{{route('profile', $user->username )}}">
+                <li class="mb-4">
+                    <div class="flex items-center text-sm">
+                        <img src="{{ $user->avatar }}" alt="" class="rounded-full mr-2 w-10">
 
-                    {{ $user->username }}
-                </div>
-            </li>
-        </a>
-        @empty
-        <p>No friends yet</p>
-        @endforelse
-    @endauth
-    
-</ul>
+                        {{ $user->username }}
+                    </div>
+                </li>
+            </a>
+            @empty
+            <li>No friends yet</li>
+            @endforelse
+        @endauth
+        
+    </ul>
+</div>
